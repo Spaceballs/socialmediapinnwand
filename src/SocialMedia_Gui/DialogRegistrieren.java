7f28139947cbd82afd7a395ec250930699ef623f
maxkobald90@googlemail.com <maxkobald90@googlemail.com@79209e9f-bc66-3a24-f16a-bc44e906524b>
2014-04-25 10:47:10 +0000

    Änderungen

--- src/SocialMedia_Gui/DialogRegistrieren.java
+++ src/SocialMedia_Gui/DialogRegistrieren.java
@@ -14,14 +14,11 @@ import java.awt.event.ActionListener;
  *
  * @author Max
  */
-public class DialogRegistrieren {
+public class DialogRegistrieren extends JFrame {
 
     public DialogRegistrieren() {
-
-        final JFrame dialog = new JFrame("Registrieren");
-
         // GridBagLayout mit Abstand zwischen den Elementen.
-        dialog.setLayout(new GridBagLayout());
+        this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.HORIZONTAL;
         c.anchor = GridBagConstraints.LINE_START;
@@ -32,60 +29,61 @@ public class DialogRegistrieren {
 
         c.gridx = 0;
         c.gridy = 0;
-        dialog.add(new JLabel("Username:", JLabel.RIGHT), c);
+        this.add(new JLabel("Username:", JLabel.RIGHT), c);
 
         JTextField username = new JTextField();
         c.gridx = 1;
         c.gridy = 0;
-        dialog.add(username, c);
+        this.add(username, c);
 
         c.gridx = 0;
         c.gridy = 1;
-        dialog.add(new JLabel("Nachname:", JLabel.RIGHT), c);
+        this.add(new JLabel("Nachname:", JLabel.RIGHT), c);
 
         JTextField name = new JTextField();
         c.gridx = 1;
         c.gridy = 1;
-        dialog.add(name, c);
+        this.add(name, c);
 
         c.gridx = 0;
         c.gridy = 2;
-        dialog.add(new JLabel("Vorname:", JLabel.RIGHT), c);
+        this.add(new JLabel("Vorname:", JLabel.RIGHT), c);
 
         JTextField surname = new JTextField();
         c.gridx = 1;
         c.gridy = 2;
-        dialog.add(surname, c);
+        this.add(surname, c);
 
         c.gridx = 0;
         c.gridy = 3;
-        dialog.add(new JLabel("Passwort:", JLabel.RIGHT), c);
+        this.add(new JLabel("Passwort:", JLabel.RIGHT), c);
 
         JPasswordField password = new JPasswordField();
         c.gridx = 1;
         c.gridy = 3;
-        dialog.add(password, c);
+        this.add(password, c);
 
 
         JButton registrieren = new JButton("Registrieren");
         c.gridx = 1;
         c.gridy = 4;
-        dialog.add(registrieren, c);
+        this.add(registrieren, c);
 
         //ActionListener Button
         registrieren.addActionListener(new ActionListener() {
            public void actionPerformed(ActionEvent e) {
-               dialog.dispose();
+               dispose();
                new DialogAnmelden();
            }
         });
 
-        dialog.pack(); //Komplettes Frame auf optimale Größe packen
+        this.setTitle("Registrieren");
+        this.pack(); //Komplettes Frame auf optimale Größe packen
         //dialog.setSize(400,400);
-        dialog.setLocationRelativeTo(null); //Position des Fensters
-        dialog.setVisible(true); //Anzeigen des Frames
+        this.setLocationRelativeTo(null); //Position des Fensters
+        this.setVisible(true); //Anzeigen des Frames
 
-        dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
 }
