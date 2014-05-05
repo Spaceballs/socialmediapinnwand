94498e078601f1c30ceb08e67e31e1b8081ceeab
Sebastian.M.Fink@googlemail.com <Sebastian.M.Fink@googlemail.com@79209e9f-bc66-3a24-f16a-bc44e906524b>
2014-05-05 14:39:28 +0000

    alle mapper fertig

--- src/SocialMedia_DatabaseManager/BeitragMapper.java
+++ src/SocialMedia_DatabaseManager/BeitragMapper.java
@@ -6,6 +6,8 @@ import java.awt.image.RescaleOp;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,6 +15,12 @@ import java.util.logging.Logger;
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.C7EFE1A2-DFDC-BDB2-0C05-445EE6027E44]
 // </editor-fold> 
+/**
+ * Class for mapping BeitragImpl objects into a sql database.
+ * 
+ * 
+ * @author Sebastian
+ */
 public class BeitragMapper extends DBStatementFactory {
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
@@ -23,12 +31,13 @@ public class BeitragMapper extends DBStatementFactory {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.3F9CA0CE-A15F-4755-D8B8-4637AD032631]
     // </editor-fold> 
-    public BeitragMapper () {
+    protected BeitragMapper () {
     }
 
     /**
-     * 
-     * @return 
+     * Singleton class of the mapper class. Either generates a new 
+     * BeitraMapper or returns the already generated Mapper Object
+     * @return BeitraMapper - Returns the AbonnementMapper Object.
      */
     public static BeitragMapper beitragMapper(){
         if(beitragMapper == null){
@@ -41,63 +50,127 @@ public class BeitragMapper extends DBStatementFactory {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.83B8D728-1072-5297-E306-A9EA071580E9]
     // </editor-fold> 
+    /**
+     * Update method of the Beitrag mapper updates object by id.
+     * 
+     * 
+     * @param val - Needs a Beitrag object without an id.
+     * @return Beitrag - Returns the inserted object with the generated id.
+     */
     public Beitrag insert (Beitrag val) {
         Connection con = DBConnection.connection();
-        return null;
+        try {
+            Timestamp t = new Timestamp(val.getCreationDate().getTime());
+            con.createStatement().executeUpdate(
+                    INSERT_INTO + " " +
+                    TABLE_NAME_BEITRAG + " ( " +
+                            COLUMN_CREATION_DATE + ", " + 
+                            COLUMN_PINNWAND_ID + ", " + 
+                            COLUMN_NUTZER_ID +  ", " + 
+                            COLUMN_TEXT + " ) " +
+                    VALUES + " ( " + 
+                            "\"" + t + "\"" + " , " + 
+                            "\"" + val.getPinnwandID() + "\"" + " , " + 
+                            "\"" + val.getNutzerID() + "\"" +  " , " + 
+                            "\"" + val.getText() + "\"" + " )");
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    SELECT + " " + 
+                            COLUMN_ID + " " + 
+                    FROM  + " " + 
+                            TABLE_NAME_BEITRAG  + " " + 
+                    ORDER_BY_ID_DESC_STATEMENT_OPTION + " " + 
+                    LIMIT_STATEMENT_OPTION + " " + "1");
+            if (resultSet.next())
+                val.setID((resultSet.getInt(COLUMN_ID)));
+        } catch (SQLException ex) {
+            Logger.getLogger(BeitragMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return val;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.4ACBD08C-1948-DCEB-5211-AC0A39939014]
     // </editor-fold> 
+    /**
+     * Updates the Beitrag object by the objects id.
+     * 
+     * 
+     * @param val - Needs an Beitrag object with a id.
+     * @return Beitrag - Returns the updated objects.
+     */
     public Beitrag update (Beitrag val) {
-        Connection con = DBConnection.connection();
+        Connection con = DBConnection.connection();   
+        try {
+            Timestamp t = new Timestamp(val.getCreationDate().getTime());
+            con.createStatement().executeUpdate(
+                    UPDATE + " " +
+                            TABLE_NAME_BEITRAG + " " +
+                            SET + " " +
+                                    COLUMN_CREATION_DATE + " =\"" + t + "\"" + ", " +
+                                    COLUMN_NUTZER_ID + " =\"" + val.getNutzerID() + "\""  + ", " +
+                                    COLUMN_PINNWAND_ID + " =\"" + val.getPinnwandID() + "\""  + " " +
+                                    COLUMN_TEXT + " =\"" + val.getText() + "\""  + " " +
+                            WHERE + " " +
+                                    COLUMN_ID + " = " + val.getID());
+        } catch (SQLException ex) {
+            Logger.getLogger(BeitragMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.5A108A9E-5162-7EDB-7A86-C17CD171A261]
     // </editor-fold> 
+    /**
+     * Deletes the given Beitrag from the sql table by id.
+     * 
+     * 
+     * @param val - Beitrag to be deleted.
+     */
     public void delete (Beitrag val) {
         Connection con = DBConnection.connection();
         try {
             con.createStatement().executeQuery(
                     DELETE + " " +
                     FROM + " " +
-                    TABLE_NAME_BEITRAG + " " +
+                            TABLE_NAME_BEITRAG + " " +
                     WHERE + " " +
-                    COLUMN_ID + "=" + "`" + val.getID() + "`" + " " +
-                    COLUMN_CREATION_DATE + "=" + "`" + val.getCreationDate() + "`" + " " +
-                    COLUMN_NUTZER_ID + "=" + "`" + val.getNutzerID() + "`" + " " +
-                    COLUMN_PINNWAND_ID + "=" + "`" + val.getPinnwandID() + "`" +  " " +
-                    COLUMN_TEXT + "=" + "`" + val.getText() + "`" + " " +
-                    ORDER_BY_ID_STATEMENT_OPTION);
+                            COLUMN_ID + " =\"" + val.getID() + "\"");
         } catch (SQLException ex) {
-            Logger.getLogger(AbonnementMapper.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(BeitragMapper.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    /**
-     * 
-     * @return 
-     */
+
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.0B0DADF1-20A3-4875-9AA5-6F90023A7F32]
     // </editor-fold> 
+    /**
+     * Method to read every entry in the Beitrag table.
+     * 
+     * 
+     * @return Vector<Beitrag> - Returns all the entrys in the sql table
+     */
     public Vector<Beitrag> getAll () {
         Connection con = DBConnection.connection();
         Vector<Beitrag> beitraege = new Vector<Beitrag>();
         try {
             ResultSet resultSet = con.createStatement().executeQuery(
                 SELECT + " " +
-                COLUMN_ID + ", " + COLUMN_CREATION_DATE + ", " + COLUMN_NUTZER_ID + ", " + COLUMN_PINNWAND_ID + ", " + COLUMN_TEXT + " " +
+                        COLUMN_ID + ", " + 
+                        COLUMN_CREATION_DATE + ", " + 
+                        COLUMN_NUTZER_ID + ", " + 
+                        COLUMN_PINNWAND_ID + ", " + 
+                        COLUMN_TEXT + " " +
                 FROM + " " +
-                TABLE_NAME_BEITRAG + " " +
+                        TABLE_NAME_BEITRAG + " " +
                 ORDER_BY_ID_STATEMENT_OPTION);
             while (resultSet.next()) {
                 try {
                     Beitrag beitrag = new BeitragImpl();
                     beitrag.setID(resultSet.getInt(COLUMN_ID));
-                    beitrag.setCreationDate(resultSet.getDate(COLUMN_CREATION_DATE));
+                    beitrag.setCreationDate(resultSet.getTimestamp(COLUMN_CREATION_DATE));
                     beitrag.setNutzerID( resultSet.getInt(COLUMN_NUTZER_ID) );
                     beitrag.setPinnwandID(resultSet.getInt(COLUMN_PINNWAND_ID));
                     beitrag.setText(resultSet.getString(COLUMN_TEXT));
@@ -117,10 +190,11 @@ public class BeitragMapper extends DBStatementFactory {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.D12ED43A-5458-0A9B-A33B-4670D16CC3F1]
     // </editor-fold> 
+    /**
     public Beitrag findByID (int val) {
         Connection con = DBConnection.connection();
         return null;
     }
-
+    */
 }
 
--- src/SocialMedia_DatabaseManager/KommentarMapper.java
+++ src/SocialMedia_DatabaseManager/KommentarMapper.java
@@ -5,6 +5,8 @@ import SocialMedia_Data.KommentarImpl;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,6 +14,12 @@ import java.util.logging.Logger;
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.86508071-818C-8C40-6662-8E8A4D4DE5AB]
 // </editor-fold> 
+/**
+ * Class for mapping KommentarImpl objects onto a sql database.
+ * 
+ * 
+ * @author Sebastian
+ */
 public class KommentarMapper extends DBStatementFactory{
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
@@ -22,10 +30,14 @@ public class KommentarMapper extends DBStatementFactory{
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.D3B5CF97-9DF3-0750-3151-0AD4546D3D35]
     // </editor-fold> 
-    public KommentarMapper () {
+    protected KommentarMapper () {
     }
 
-
+    /**
+     * Singleton class of the mapper class. Either generates a new 
+     * KommentarMapper or returns the already generated Mapper Object
+     * @return KommentarMapper - Returns the AbonnementMapper Object.
+     */
     public static KommentarMapper kommentarMapper(){
         if (kommentarMapper == null) {
             kommentarMapper = new KommentarMapper();
@@ -37,72 +49,135 @@ public class KommentarMapper extends DBStatementFactory{
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.7866B8A7-10AA-8192-AC55-B9F8AF1AE130]
     // </editor-fold> 
+    /**
+     * Update method of the Kommentar mapper updates object by id.
+     * 
+     * 
+     * @param val - Needs a Kommentar object without an id.
+     * @return Kommentar - Returns the inserted object with the generated id.
+     */    
     public Kommentar insert (Kommentar val) {
         Connection con = DBConnection.connection();
-        return null;
+        try {
+            Timestamp t = new Timestamp(val.getCreationDate().getTime());
+            con.createStatement().executeUpdate(
+                    INSERT_INTO + " " +
+                    TABLE_NAME_KOMMENTAR + " ( " +
+                            COLUMN_CREATION_DATE + ", " + 
+                            COLUMN_BEITRAG_ID + ", " + 
+                            COLUMN_NUTZER_ID +  ", " + 
+                            COLUMN_TEXT + " ) " +
+                    VALUES + " ( " + "\"" +
+                    t + "\"" + " , " + "\"" + val.getBeitragID() + "\"" + " , " + "\"" + val.getNutzerID() + "\"" +  " , " + "\"" + val.getText() + "\"" + " )");
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    SELECT + " " + 
+                            COLUMN_ID + " " + 
+                    FROM  + " " + 
+                            TABLE_NAME_KOMMENTAR  + " " + 
+                    ORDER_BY_ID_DESC_STATEMENT_OPTION + " " + 
+                    LIMIT_STATEMENT_OPTION + " " + "1");
+            if (resultSet.next())
+                val.setID((resultSet.getInt(COLUMN_ID)));
+        } catch (SQLException ex) {
+            Logger.getLogger(KommentarMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return val;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.D59859BD-8C69-DEF0-2252-03A01A7EB350]
     // </editor-fold> 
+    /**
+     * Updates the Kommentar object by the objects id.
+     * 
+     * 
+     * @param val - Needs an Kommentar object with a id.
+     * @return Kommentar - Returns the updated objects.
+     */
     public Kommentar update (Kommentar val) {
         Connection con = DBConnection.connection();
-        return null;
+        try {
+            Timestamp t = new Timestamp(val.getCreationDate().getTime());
+            con.createStatement().executeUpdate(
+                    UPDATE + " " +
+                            TABLE_NAME_KOMMENTAR + " " +
+                            SET + " " +
+                                    COLUMN_CREATION_DATE + " =\"" + t + "\"" + ", " +
+                                    COLUMN_NUTZER_ID + " =\"" + val.getNutzerID() + "\""  + ", " +
+                                    COLUMN_BEITRAG_ID + " =\"" + val.getBeitragID() + "\""  + " " +
+                                    COLUMN_TEXT + " =\"" + val.getText() + "\""  + " " +
+                            WHERE + " " +
+                                    COLUMN_ID + " = " + val.getID());
+        } catch (SQLException ex) {
+            Logger.getLogger(KommentarMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return val;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.EE3BF1F0-F3A0-F559-4A32-A1057B153199]
     // </editor-fold> 
+    /**
+     * Deletes the given Kommentar from the sql table by id.
+     * 
+     * 
+     * @param val - Kommentar to be deleted.
+     */
     public void delete (Kommentar val) {
         Connection con = DBConnection.connection();
         try {
             con.createStatement().executeQuery(
                     DELETE + " " +
                     FROM + " " +
-                    TABLE_NAME_KOMMENTAR + " " +
+                            TABLE_NAME_KOMMENTAR + " " +
                     WHERE + " " +
-                    COLUMN_ID + "=" + "`" + val.getID() + "`" + " " +
-                    COLUMN_CREATION_DATE + "=" + "`" + val.getCreationDate() + "`" + " " +
-                    COLUMN_NUTZER_ID + "=" + "`" + val.getNutzerID() + "`" + " " +
-                    COLUMN_BEITRAG_ID + "=" + "`" + val.getBeitragID() + "`" +  " " +
-                    COLUMN_TEXT + "=" + "`" + val.getText() + "`" + " " +
-                    ORDER_BY_ID_STATEMENT_OPTION);
+                            COLUMN_ID + " =\"" + val.getID() + "\"");
         } catch (SQLException ex) {
-            Logger.getLogger(AbonnementMapper.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(KommentarMapper.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.42931436-6E5A-7157-C029-F81BDF5F8CF7]
     // </editor-fold> 
+    /**
+     * Method to read every entry in the Kommentar table.
+     * 
+     * 
+     * @return Vector<Kommentar> - Returns all the entrys in the sql table
+     */
     public Vector<Kommentar> getAll () {
         Connection con = DBConnection.connection();
         Vector<Kommentar> kommentare = new Vector<Kommentar>();
         try {
             ResultSet resultSet = con.createStatement().executeQuery(
                 SELECT + " " +
-                COLUMN_ID + ", " + COLUMN_CREATION_DATE + ", " + COLUMN_NUTZER_ID + ", " + COLUMN_BEITRAG_ID + ", " + COLUMN_TEXT + " " +
+                        COLUMN_ID + ", " + 
+                        COLUMN_CREATION_DATE + ", " + 
+                        COLUMN_NUTZER_ID + ", " + 
+                        COLUMN_BEITRAG_ID + ", " + 
+                        COLUMN_TEXT + " " +
                 FROM + " " +
-                TABLE_NAME_KOMMENTAR + " " +
+                        TABLE_NAME_KOMMENTAR + " " +
                 ORDER_BY_ID_STATEMENT_OPTION);
-
             while (resultSet.next()) {
                 try {
                     Kommentar kommentar = new KommentarImpl();
                     kommentar.setID(resultSet.getInt(COLUMN_ID));
-                    kommentar.setCreationDate(resultSet.getDate(COLUMN_CREATION_DATE));
+                    kommentar.setCreationDate(resultSet.getTimestamp(COLUMN_CREATION_DATE));
                     kommentar.setNutzerID( resultSet.getInt(COLUMN_NUTZER_ID) );
                     kommentar.setBeitragID(resultSet.getInt(COLUMN_BEITRAG_ID));
                     kommentar.setText(resultSet.getString(COLUMN_TEXT));
                     kommentare.addElement(kommentar);
                 }
                 catch(SQLException e) {
-                    e.printStackTrace();
+                    Logger.getLogger(KommentarMapper.class.getName()).log(Level.SEVERE, null, e);
                     break;
                 }
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            Logger.getLogger(KommentarMapper.class.getName()).log(Level.SEVERE, null, e);
         }
         return kommentare;
     }
@@ -110,10 +185,11 @@ public class KommentarMapper extends DBStatementFactory{
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.5A7836AD-406C-422D-ADA6-76B6D52AF31F]
     // </editor-fold> 
+    /**
     public Kommentar findByID (int val) {
         Connection con = DBConnection.connection();
         return null;
     }
-
+    */
 }
 
