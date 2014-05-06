dc0cf334f092f522d726001cfd49c4bfe656cde3
Sebastian.M.Fink@googlemail.com <Sebastian.M.Fink@googlemail.com@79209e9f-bc66-3a24-f16a-bc44e906524b>
2014-05-06 15:28:47 +0000

    Owner Check in logik implementiert

--- src/SocialMedia_Report/Report.java
+++ src/SocialMedia_Report/Report.java
@@ -8,41 +8,62 @@ import java.util.Vector;
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.327FB869-2B98-9C14-B7F3-E4CCB7B95F64]
 // </editor-fold> 
+/**
+ * 
+ * @author Sebastian
+ */
 public class Report {
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.C01546F6-1230-9D1A-E8B7-1C6BA18AA572]
     // </editor-fold> 
     private String reportTitle = "Report";
+    
+    private String imprint = "";
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.DD0CA414-A7D3-3EE0-28C4-6DDA3EBB3A5F]
     // </editor-fold> 
     private Date creationDate = new Date();
+    
+    private Date startDate;
+            
+    private Date endDate;
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.6C37A6A4-26A4-83AC-CC6C-E9599681DD35]
     // </editor-fold> 
-    private Vector<Row> sRows = null;
+    private Vector<Row> rows = null;
 
-    private Vector<Paragraph> mParagraphs = null;
+    private Vector<Paragraph> paragraphs = null;
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.3A56DCBA-88E9-DEDF-0689-5B42C1C3471E]
     // </editor-fold> 
+    /**
+     * 
+     */
     public Report () {
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.736AE163-DD52-AE45-73DA-F720D296BEBF]
     // </editor-fold> 
+    /**
+     * 
+     * @return 
+     */
     public Date getCreationDate () {
-        return creationDate;
+        return this.creationDate;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.DF9B67FA-09D6-BC6D-AFAD-6C11C9D79470]
     // </editor-fold> 
+    /**
+     * 
+     * @param val 
+     */
     public void setCreationDate (Date val) {
         this.creationDate = val;
     }
@@ -55,7 +76,7 @@ public class Report {
      * @return 
      */
     public String getReportTitle () {
-        return reportTitle;
+        return this.reportTitle;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
@@ -77,7 +98,7 @@ public class Report {
      * @param val 
      */
     public void addRow (Row val) {
-        sRows.add(val);
+        this.rows.add(val);
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
@@ -88,7 +109,7 @@ public class Report {
      * @param val 
      */
     public void removeRow (Row val) {
-        sRows.remove(val);
+        this.rows.remove(val);
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
@@ -99,7 +120,7 @@ public class Report {
      * @return 
      */
     public Vector<Row> getAllRows () {
-        return sRows;
+        return this.rows;
     }
     
     /**
@@ -107,23 +128,71 @@ public class Report {
      * @return 
      */
     public Vector<Paragraph> getAllParagraphs() {
-        return mParagraphs;
+        return this.paragraphs;
     }
 
     /**
      * 
-     * @param mParagraph 
+     * @param Paragraph 
      */
-    public void removeParagraph(Paragraph mParagraph) {
-        this.mParagraphs.remove(mParagraph);
+    public void removeParagraph(Paragraph Paragraph) {
+        this.paragraphs.remove(Paragraph);
     }
     
     /**
      * 
-     * @param mParagraph 
+     * @param Paragraph
      */
-    public void setParagraph(Paragraph mParagraph) {
-        mParagraphs.add(mParagraph);
+    public void setParagraph(Paragraph Paragraph) {
+        this.paragraphs.add(Paragraph);
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getImprint() {
+        return imprint;
+    }
+
+    /**
+     * 
+     * @param imprint 
+     */
+    public void setImprint(String imprint) {
+        this.imprint = imprint;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * 
+     * @param startDate 
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * 
+     * @param endDate 
+     */
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
     }
 }
 
