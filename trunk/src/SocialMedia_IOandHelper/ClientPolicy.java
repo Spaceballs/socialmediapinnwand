0bc776582dfe2f24d0ac6114a4a7dd43b878cb51
Sebastian.M.Fink@googlemail.com <Sebastian.M.Fink@googlemail.com@79209e9f-bc66-3a24-f16a-bc44e906524b>
2014-06-12 10:25:09 +0000

    aufräumen

---
+++ trunk/src/SocialMedia_IOandHelper/ClientPolicy.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SocialMedia_IOandHelper;
+
+import java.awt.AWTPermission;
+import java.io.FilePermission;
+import java.net.SocketPermission;
+import java.security.CodeSource;
+import java.security.PermissionCollection;
+import java.security.Policy;
+import java.util.PropertyPermission;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class ClientPolicy extends Policy {
+
+    private static PermissionCollection perms;
+
+    public ClientPolicy() {
+        super();
+        if (perms == null) {
+            perms = new MyPermissionCollection();
+            addPermissions();
+        }
+    }
+
+    @Override
+    public PermissionCollection getPermissions(CodeSource codesource) {
+        return perms;
+    }
+
+    private void addPermissions() {
+        SocketPermission socketPermission = new SocketPermission("*:1024-", "connect, resolve");
+        PropertyPermission propertyPermission = new PropertyPermission("<<ALL FILES>>", "read, write");
+        PropertyPermission propertyPermission0 = new PropertyPermission("line.separator", "read");
+        FilePermission filePermission = new FilePermission("<<ALL FILES>>", "read, write, execute");
+        AWTPermission aWTPermission = new AWTPermission("showWindowWithoutWarningBanner");
+        
+        perms.add(aWTPermission);
+        perms.add(socketPermission);
+        perms.add(propertyPermission);
+        perms.add(filePermission);
+    }
+
+}
---
+++ trunk/src/SocialMedia_IOandHelper/ServerPolicy.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SocialMedia_IOandHelper;
+
+import java.io.FilePermission;
+import java.net.SocketPermission;
+import java.security.CodeSource;
+import java.security.PermissionCollection;
+import java.security.Policy;
+import java.util.PropertyPermission;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class ServerPolicy extends Policy {
+
+    private static PermissionCollection perms;
+
+
+    
+    
+    public ServerPolicy() {
+        super();
+        if (perms == null) {
+            perms = new MyPermissionCollection();
+            addPermissions();
+        }
+    }
+
+    @Override
+    public PermissionCollection getPermissions(CodeSource codesource) {
+        return perms;
+    }
+
+    private void addPermissions() {
+        RuntimePermission rtp0 = new RuntimePermission("setSecurityManager");
+        RuntimePermission rtp1 = new RuntimePermission("createSecurityManager");
+        RuntimePermission rtp2 = new RuntimePermission("usePolicy");
+        RuntimePermission rtp3 = new RuntimePermission("accessClassInPackage.com.mysql.jdbc.Driver");
+        PropertyPermission propertyPermission0 = new PropertyPermission("line.separator", "read");
+        FilePermission fp = new  FilePermission("<<ALL FILES>>", "execute");
+        PropertyPermission pp = new PropertyPermission("java.rmi.registry.hostname", "read");
+        SocketPermission sp = new SocketPermission("*:1024-", "accept, connect, resolve");
+        SocketPermission sp0 = new SocketPermission("*:1521-", "connect, resolve");
+        
+        perms.add(propertyPermission0);
+        perms.add(fp);
+        perms.add(pp);
+        perms.add(sp);
+        perms.add(sp0);
+        perms.add(rtp0);
+        perms.add(rtp1);
+        perms.add(rtp2);
+        perms.add(rtp3);
+    }
+
+}
