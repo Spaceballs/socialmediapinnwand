791cb2fd7a1a1142716c4d82b225a49fcb957074
Sebastian.M.Fink@googlemail.com <Sebastian.M.Fink@googlemail.com@79209e9f-bc66-3a24-f16a-bc44e906524b>
2014-06-12 10:25:09 +0000

    aufräumen

---
+++ src/SocialMedia_IOandHelper/MyPermissionCollection.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SocialMedia_IOandHelper;
+
+import java.security.Permission;
+import java.security.PermissionCollection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Sebastian
+ */
+class MyPermissionCollection extends PermissionCollection {
+
+    private static final long serialVersionUID = 614300921365729272L;
+
+    ArrayList<Permission> perms = new ArrayList<Permission>();
+
+    public void add(Permission p) {
+        perms.add(p);
+    }
+
+    public boolean implies(Permission p) {
+        for (Iterator<Permission> i = perms.iterator(); i.hasNext();) {
+            if (((Permission) i.next()).implies(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Enumeration<Permission> elements() {
+        return Collections.enumeration(perms);
+    }
+
+    @Override
+    public boolean isReadOnly() {
+        return false;
+    }
+
+}
